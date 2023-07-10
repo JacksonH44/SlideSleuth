6e7d9da341d5abd6c63b7ed64c6e2faf3fdfef69
Jackson Howe <jhowe4@gra-login3.graham.sharcnet>
2023-07-10 15:32:48 -0400

    Transfer learning large scale pipeline test

--- jobs/transfer_learning.sh
+++ jobs/transfer_learning.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-sushant
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
-#SBATCH --time=0-10:00:00
-#SBATCH --mem=32G
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=80G
 #SBATCH --job-name=transfer_learning
 #SBATCH --output=../outputs/SLURM_DEFAULT_OUT/transfer_learning-%j.out
 #SBATCH --error=../outputs/SLURM_DEFAULT_OUT/transfer_learning-%j.err
@@ -16,9 +17,10 @@ virtualenv --no-download $ENVDIR
 module load cuda/11.7 cudnn
 module load scipy-stack
 source $ENVDIR/bin/activate
-pip install --no-index tensorflow
-pip install --no-index pillow
-pip install -q -U --no-index scikit-learn
+pip install --no-index --upgrade pip
+pip install -q --no-index tensorflow
+pip install -q --no-index pillow
+pip install -q --no-index scikit-learn
 python ../src/transfer_learning.py
 deactivate
 rm -rf $ENVDIR
--- src/transfer_learning.py
+++ src/transfer_learning.py
@@ -4,7 +4,7 @@
   
   Author: Jackson Howe
   Date Created: June 24, 2023
-  Last Updated: June 27, 2023
+  Last Updated: July 7, 2023
   '''
 
 import tensorflow as tf
@@ -21,11 +21,85 @@ import PIL
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve
 
+import math
+import os
+import tempfile
+from datetime import datetime
+import pickle
+from optparse import OptionParser
+import sys
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 # tf.compat.v1.disable_eager_execution()
 
 LABEL = '/scratch/jhowe4/outputs/GDC/brca_example_10x/labels.csv'
 ERR_FILE = '/scratch/jhowe4/outputs/GDC/brca_example_10x/corrupt_images_log.txt'
+WEIGHTS_PATH = '../model/weights'
+
+EPOCHS = 5
+BATCH_SIZE = 2048
+NUM_TRAINING_TILES = 203110
+NUM_POS_TRAINING = 175410
+NUM_NEG_TRAINING = 27700
+NUM_VALID_TILES = 15855
+STEPS_PER_EPOCH = math.ceil(NUM_TRAINING_TILES / BATCH_SIZE)
+VALIDATION_STEPS = math.ceil(NUM_VALID_TILES / BATCH_SIZE)
+
+METRICS = [
+    tf.keras.metrics.TruePositives(name='tp'),
+    tf.keras.metrics.FalsePositives(name='fp'),
+    tf.keras.metrics.TrueNegatives(name='tn'),
+    tf.keras.metrics.FalseNegatives(name='fn'),
+    tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc'),
+    tf.keras.metrics.AUC(name='prc', curve='PR') # Precision-recall curve
+]
+
+def is_opt_provided(parser, dest):
+  """Determines if an argument to the option parser is provided
+
+  Args:
+      parser (OptionParser): Parser
+      dest (str): Option destination of interest
+  """
+  
+  if any (opt.dest == dest and (opt._long_opts[0] in sys.argv[1:] or opt._short_opts[0] in sys.argv[1:]) for opt in parser._get_all_options()):
+    return True
+  return False  
+
+def save_model(model, save_dir):
+  """A function that makes the directory in which the weights will be stored in 
+  and writes it to a unique file
+
+  Args:
+      model (tf.keras.models.Model): The model you wish to save
+      weights_dir (str): The path to the directory in which the weights will be 
+      saved
+  """
+  
+  # Make the folder if it doesn't exist
+  if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+    
+  # Get a unique file name
+  now = datetime.now()
+  date_time = now.strftime("%m-%d-%Y_%H-%M-%S")
+  
+  # Save model weights
+  model.save_weights(os.path.join(save_dir, f'weights_{date_time}.h5'))
+  
+  # Save model parameters
+  parameters = [
+      BATCH_SIZE,
+      EPOCHS
+    ]
+    
+  # Dump into a pickle file
+  save_path = os.path.join(save_dir, f"parameters_{date_time}.pkl")
+  with open(save_path, "wb") as f:
+    pickle.dump(parameters, f)
     
 def organize_dir(dir_path):
   """the result of tile_[type].sh is the following directory structure:
@@ -113,6 +187,7 @@ def clean_datasets(dir_path):
   Args:
       dir_path (String): path to directory that needs to be cleaned
   """
+  
   for label in listdir(dir_path):
     for img_file in listdir(join(dir_path, label)):
       try:
@@ -122,7 +197,58 @@ def clean_datasets(dir_path):
         with open(ERR_FILE, 'a') as err_file:
           err_file.write(f"Removed fie {join(dir_path, label, img_file)} because it was unreadable")
           
+def make_model(metrics=METRICS, output_bias=None):
+  """A functiont that makes a transfer learning model
+
+  Args:
+      metrics (list<tf.keras.metrics>, optional): A list of metrics for 
+      tensorflow to track during training. Defaults to METRICS.
+      output_bias (np.array, optional): Output bias to put on the last layer (1 
+      neuron dense layer). Defaults to None.
+
+  Returns:
+      tf.keras.models.Model: The built tensorflow model
+  """
+  
+  if output_bias is not None:
+    output_bias = tf.keras.initializers.Constant(output_bias)
+  model = tf.keras.Sequential()
+    
+  # Add ResNet50 model
+  model.add(tf.keras.applications.ResNet50(include_top = False, pooling = 'avg', weights ='imagenet'))
+    
+  # Add dense layers
+  model.add(tf.keras.layers.Dense(1024, activation='relu'))
+  model.add(tf.keras.layers.Dense(256, activation='relu'))
+  model.add(tf.keras.layers.Dense(32, activation='relu'))
+  
+  # Add outer classification layer to model
+  model.add(tf.keras.layers.Dense(1, activation = 'sigmoid', bias_initializer=output_bias))
+  
+  # Not training the resnet on the new data set. Using the pre-trained weigths
+  model.layers[0].trainable = False 
+    
+  model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=metrics
+  )
+    
+  return model
+          
 def extract_labels(ds):
+  """A function that gets the labels for each observation in the dataset. Use 
+  this function when you get need the labels of observations that are in a 
+  dataset returned from the flow_from_directory method of the 
+  ImageDataGenerator class
+
+  Args:
+      ds (tf.data.Dataset): The dataset you want to extract labels from
+
+  Returns:
+      np.array: Vector of true dataset labels
+  """
+  
   labels = []
   num_batches = len(ds)
   for batch_idx in range(num_batches):
@@ -132,36 +258,97 @@ def extract_labels(ds):
   labels = np.array(labels)
   return labels 
 
-def plot_roc_curve(y_true, y_pred):
+def plot_roc(name, labels, predictions, **kwargs):
+  """Plots ROC curve based off false and true positive frequencies. From https://www.tensorflow.org/tutorials/structured_data/imbalanced_data.
+  
+  Args:
+      name (str): Label name
+      labels (np.array): Ground truth 
+      predictions (np.array): Trained model predictions
   """
-  plots the roc curve based off the probabilities
-  """
+  
+  fp, tp, _ = roc_curve(labels, predictions)
     
-  fpr, tpr, _ = roc_curve(y_true, y_pred)
-  plt.plot(fpr, tpr)
-  plt.xlabel('False Positive Rate')
-  plt.label('True Positive Rate')
-  plt.savefig('../img/roc.pdf')
+  plt.plot(100*fp, 100*tp, label=name, linewidth=2, **kwargs)
+  plt.xlabel('False positives [%]')
+  plt.ylabel('True positives [%]')
+  plt.xlim([-0.5, 100.5])
+  plt.ylim([-0.5, 100.5])
+  plt.grid(True)
+  ax = plt.gca()
+  ax.set_aspect('equal')
+
+def plot_metrics(history):
+  """Plot a variety of model metrics. From https://www.tensorflow.org/tutorials/
+  structured_data/imbalanced_data.
+
+  Args:
+      history (tf.keras.History): Trained model history
+  """
+  
+  metrics = ['loss', 'prc', 'precision', 'recall']
+  
+  for n, metric in enumerate(metrics):
+    name = metric.replace("_"," ").capitalize()
+    
+    plt.subplot(2,2,n+1)
+    plt.plot(
+      history.epoch, 
+      history.history[metric], 
+      color='lightcoral', 
+      label='Train'
+    )
+    plt.plot(
+      history.epoch, 
+      history.history['val_'+metric],
+      color='lightcoral', 
+      linestyle="--", 
+      label='Val'
+    )
+    
+    plt.xlabel('Epoch')
+    plt.ylabel(name)
+    
+    if metric == 'loss':
+      plt.ylim([0, plt.ylim()[1]])
+    elif metric == 'auc':
+      plt.ylim([0.6,1])
+    else:
+      plt.ylim([0,1])
+
+    plt.legend()
+    plt.tight_layout()
 
 if __name__ == '__main__':
+  # User can specify if they want to preprocess their data, or if its already processed into train/, test/, valid/, and 0/, 1/ within each
+  parser = OptionParser(usage='Usage: %prog options')
+  parser.add_option(
+    '-P',
+    '--preprocess',
+    dest='preprocessing directive',
+    default=False,
+    help='preprocess image data before running transfer learning'
+  )
+  
   # Make sure the error file is clean
-  # if exists(ERR_FILE):
-  #   remove(ERR_FILE)
+  if exists(ERR_FILE):
+    remove(ERR_FILE)
     
   dir_path = '/scratch/jhowe4/outputs/GDC/brca_example_10x'
   train_path = join(dir_path, 'train')
   test_path = join(dir_path, 'test')
   valid_path = join(dir_path, 'valid')
   
-  # # Organize the data directory
-  # organize_dir(train_path)
-  # organize_dir(test_path)
-  # organize_dir(valid_path)
+  if is_opt_provided(parser, 'preprocessing directive'):
+    # Organize the data directory
+    organize_dir(train_path)
+    organize_dir(test_path)
+    organize_dir(valid_path)
   
-  # # Clean each of its subfolders
-  # clean_datasets(train_path)
-  # clean_datasets(test_path)
-  # clean_datasets(valid_path)
+    # Clean each of its subfolders
+    clean_datasets(train_path)
+    clean_datasets(test_path)
+    clean_datasets(valid_path)
 
   # Create data generator
   datagen = ImageDataGenerator(
@@ -174,7 +361,7 @@ if __name__ == '__main__':
     train_path,
     target_size=(224, 224),
     class_mode='binary',
-    batch_size=32
+    batch_size=BATCH_SIZE
   )
   
   # Testing dataset
@@ -182,7 +369,7 @@ if __name__ == '__main__':
     test_path,
     target_size=(224, 224),
     class_mode='binary',
-    batch_size=32
+    batch_size=BATCH_SIZE
   )
   
   # Validation dataset
@@ -190,54 +377,83 @@ if __name__ == '__main__':
     valid_path,
     target_size=(224, 224),
     class_mode='binary',
-    batch_size=32
+    batch_size=BATCH_SIZE
   )
   
-  # # Build model
-  # model = tf.keras.Sequential()
+  # Total number of positive and negative samples in the training dataset
+  pos = NUM_POS_TRAINING
+  neg = NUM_NEG_TRAINING
+  
+  total = pos + neg
+  print(f"Number of Positive Samples: {pos}\nNumber of Negative Samples: {neg}")
+  weight_for_0 = (1 / neg) * (total / 2.0)
+  weight_for_1 = (1 / pos) * (total / 2.0)
 
-  # # Transfer learning
-  # model.add(tf.keras.applications.ResNet50(include_top = False, pooling = 'avg', weights ='imagenet'))
+  class_weight = {0: weight_for_0, 1: weight_for_1}
+  print('Weight for class 0: {:.2f}' .format(weight_for_0))
+  print('Weight for class 1: {:.2f}' .format(weight_for_1))
+
+  initial_bias = np.log([pos / neg])
+
+  # Checkpoint the initial weights
+  model = make_model(output_bias=initial_bias)
+  initial_weights = os.path.join(tempfile.mkdtemp(), 'initial_weights')
+  model.save_weights(initial_weights)
   
-  # # 2nd layer as Dense for 2-class classification
-  # model.add(tf.keras.layers.Dense(2, activation = 'sigmoid'))
+  # Create a callback to stop when the model converges
+  early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_prc',
+    verbose=1,
+    patience=10,
+    mode='max',
+    restore_best_weights=True
+  )
   
-  # # Not training the resnet on the new data set. Using the pre-trained weigths
-  # model.layers[0].trainable = False  
+  # Fit the model
+  history = model.fit(
+    train_ds,
+    epochs=EPOCHS,
+    steps_per_epoch=STEPS_PER_EPOCH,
+    validation_data=valid_ds,
+    validation_steps=VALIDATION_STEPS,
+    callbacks=[early_stopping],
+    class_weight=class_weight
+  )
   
-  # # compile the model
-  # model.compile(
-  #   optimizer = 'adam', 
-  #   loss = 'binary_crossentropy', 
-  #   metrics = [tf.keras.metrics.AUC()])
+  # Save the model weights
+  save_model(model, WEIGHTS_PATH)
   
-  # # Fit the model
-  # fit_history = model.fit(
-  #   train_ds,
-  #   steps_per_epoch=4,
-  #   validation_data=valid_ds,
-  #   validation_steps=4,
-  #   epochs=3,
-  # )
+  # Evaluate model
+  eval_result = model.evaluate(
+    test_ds,
+    steps=len(test_ds)
+  )
   
-  # # Save the model weights
-  # weights_path = '../model/weights/tf-2023-06-30_weights.h5'
-  # weights_dir = dirname(weights_path)
-  # if not isdir(weights_dir):
-  #   makedirs(weights_dir)
-  # model.save_weights(weights_path)
+  # Plot the ROC curve
+  train_predictions_baseline = model.predict(train_ds, batch_size=BATCH_SIZE)
+  test_predictions_baseline = model.predict(test_ds, batch_size=BATCH_SIZE)
   
-  # # Evaluate model
-  # eval_result = model.evaluate(
-  #   test_ds,
-  #   steps=len(test_ds)
-  # )
-  # print("[test loss, test accuracy]:", eval_result)
-  
-  # y_pred = model.predict(test_ds)
-  # y_true = extract_labels(test_ds)
-  
-  # print(f"Predicted labels: {len(y_pred)}")
-  # print(f"True labels: {len(y_true)}")
+  train_labels = extract_labels(train_ds)
+  test_labels = extract_labels(test_ds)
     
-  # plot_roc_curve(y_true, y_pred)
+  plot_roc(
+    "Train Baseline", 
+    train_labels, 
+    train_predictions_baseline, 
+    color='deepskyblue'
+  )
+  plot_roc(
+    "Test Baseline", 
+    test_labels, 
+    test_predictions_baseline, 
+    color='deepskyblue', 
+    linestyle='--'
+  )
+  plt.legend(loc='lower right')
+  plt.savefig('../img/roc_curve.png')
+  plt.close()
+  
+  # Plot other model metrics
+  plot_metrics(history)
+  plt.savefig('../img/plot_metrics.pdf')
+  plt.close()
