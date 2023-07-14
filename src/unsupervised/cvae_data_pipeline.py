33a78c63129cb85d1597f6f6f90d024711801133
Jackson Howe <jhowe4@gra-login1.graham.sharcnet>
2023-07-14 09:56:40 -0400

    finish data preprocessing script for cvae

--- src/unsupervised/cvae_data_pipeline.py
+++ src/unsupervised/cvae_data_pipeline.py
@@ -20,7 +20,29 @@ AUTOTUNE = tf.data.AUTOTUNE
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def organize_root(root_dir):
+def organize_dir(root_dir):
+  """A function that changes directory structure from:
+  
+  root_dir/
+    001_files/
+      5.0/
+        img1.jpeg
+        ...
+        imgN.jpeg
+    ...
+    100_files/
+      ...
+    
+  to:
+  
+  root_dir/
+    001-5.0-im1.jpeg
+    ...
+    100-5.0-imN.jpeg
+
+  Args:
+      root_dir (str): The path to the root directory you wish to be reorganized
+  """
   for sub_dir in listdir(root_dir):
     if isdir(join(root_dir, sub_dir)):
       for mag in listdir(join(root_dir, sub_dir)):
@@ -28,7 +50,8 @@ def organize_root(root_dir):
           new_img_name = f"{sub_dir.split('_')[0]}-{mag}-{img}"
           move(f'{join(root_dir, sub_dir, mag, img)}', f'{join(root_dir, new_img_name)}')
           
-  # Go through and remove the other files from the directory that are not in use anymore
+  # Go through and remove the other files from the directory that are not in #
+  # use anymore (keep any .jpeg and the error log file)
   for file in listdir(root_dir):
     full_file = join(root_dir, file)
     if not (full_file == ERR_FILE or full_file.split(".")[-1] == "jpeg"):
@@ -52,50 +75,38 @@ def clean_directory(clean_dir, log_file=ERR_FILE):
       with open(log_file, 'a') as err:
         err.write(f"{join(clean_dir, file)} removed because it was corrupt.")
       remove(join(clean_dir, file))
-        
-def clean_dataset_from_root(clean_root):
-  """cleans a whole dataset of corrupt images. Expects a directory structure of:
+      
+def preprocess_directory(root_path):
+  """A function that cleans and reorganizes a directory for convolutional 
+  variational autoencoder. Assumes a structure of:
   
-  root_dir/
-    001_files/
-      5.0/
-        10_10.jpeg
-        10_11.jpeg
-        ...
-      10.0/
-        ...
-    002A_files/
-      ...
-    98C_files/
-      ...
+  root_path/
+    train/
+    test/
+    valid/
+    
+  With each of train, test, and valid having directory structure as assumed in 
+  organize_root
 
   Args:
-      clean_root (str): expects the root of the directory 
+      root_path (str): The path to the root directory
   """
+  train_path = join(root_path, 'train')
+  valid_path = join(root_path, 'valid')
+  test_path = join(root_path, 'test')
   
-  for file in listdir(clean_root):
-    if isdir(join(clean_root, file)):
-      for mag in listdir(join(clean_root, file)):
-        print(f"Cleaning {join(clean_root, file, mag)}")
-        clean_directory(join(clean_root, file, mag))
+  # Train
+  organize_dir(train_path)
+  clean_directory(train_path)
   
-def make_dataset(dir_path, img_size=(IMG_SIZE, IMG_SIZE), shuffle=True):
-  """makes the dataset for a directory of images. Assumes the directory has already been cleaned, or else the error: 'InvalidArgumentError: Input is empty.' may pop up
-
-  Returns:
-      tf.data.Dataset: a tensorflow dataset object representing all images in the directory
-  """
+  # Valid
+  organize_dir(valid_path)
+  clean_directory(valid_path)
   
-  datagen = tf.keras.preprocessing.image.ImageDataGenerator()
-  return datagen.flow_from_directory(
-    dir_path,
-    target_size=img_size,
-    classes=None,
-    class_mode=None,
-    batch_size=BATCH_SIZE,
-    shuffle=shuffle
-  )
+  # Test
+  organize_dir(test_path)
+  clean_directory(test_path)
 
 if __name__ == '__main__':
   dir_path = '../outputs/HNE'
-  dataset = make_dataset(dir_path)
+  preprocess_directory(dir_path)
