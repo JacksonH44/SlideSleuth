3868e0d69108d3f443736ed5b6da02e37facc529
Jackson Howe <jhowe4@gra-login1.graham.sharcnet>
2023-07-31 15:15:53 -0400

    ignore folder_utils.sh

--- src/data/folder_utils.sh
+++ src/data/folder_utils.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-sushant
-#SBATCH --mem=65536M
-#SBATCH --time=0-01:00:00
+#SBATCH --mem=102400M
+#SBATCH --time=0-08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --job-name=folder_utils
 #SBATCH --output=../../SLURM_DEFAULT_OUT/folder_utils-%j.out
 #SBATCH --error=../../SLURM_DEFAULT_OUT/folder_utils-%j.err
 
-cp -r ../../data/interim/HNE_images ../../data/processed/HNE
+rm -r ../../data/interim/CK7
